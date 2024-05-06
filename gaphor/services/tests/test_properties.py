491eeb3f373bb71f0a1257b654149c11f3059218
Dan Yeaw <dan@yeaw.me>
2024-05-05 21:56:50 -0400

    Merge pull request #3295 from mikekidner/script_docs_addition
    added further explanation of updating existing elements in a model

---
+++ gaphor/services/tests/test_properties.py
@@ -0,0 +1,54 @@
+from pathlib import Path
+
+import pytest
+
+import gaphor.services.properties
+from gaphor.event import ModelLoaded, ModelSaved
+from gaphor.services.properties import get_cache_dir, get_config_dir
+
+
+@pytest.fixture
+def properties(event_manager):
+    properties = gaphor.services.properties.Properties(event_manager)
+    yield properties
+    properties.shutdown()
+
+
+def test_set_property(properties):
+    properties.on_model_loaded(ModelLoaded(None, "some_file_name"))
+
+    properties.set("test", 1)
+
+    assert properties.get("test") == 1
+
+
+def test_load_properties(properties, event_manager):
+    properties.set("test", 1)
+    properties.on_model_saved(ModelSaved(None, "test_load_properties"))
+
+    new_properties = gaphor.services.properties.Properties(event_manager)
+    new_properties.on_model_loaded(ModelLoaded(None, "test_load_properties"))
+
+    assert new_properties.get("test") == 1
+
+
+def test_load_of_corrupted_properties(properties, event_manager, caplog):
+    properties.set("test", 1)
+    properties.on_model_saved(ModelSaved(None, "test_load_properties"))
+    Path(properties.filename).write_text("{ invalid content }", encoding="utf-8")
+
+    new_properties = gaphor.services.properties.Properties(event_manager)
+    new_properties.on_model_loaded(ModelLoaded(None, "test_load_properties"))
+
+    assert new_properties.get("test", "not set") == "not set"
+    assert "Invalid syntax in property file" in caplog.text
+
+
+def test_config_dir():
+    config_dir = get_config_dir()
+    assert str(config_dir).endswith("gaphor")
+
+
+def test_cache_dir():
+    cache_dir = get_cache_dir()
+    assert str(cache_dir).endswith("gaphor")
